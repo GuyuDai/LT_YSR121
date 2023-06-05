e69ec22aec752b1fbf69f1fe827caf749cfebb11
Yixue Dai <Yixue.Dai@campus.lmu.de>
2023-06-05 19:34:02 +0800

    huixingshu

--- V4/src/Main.java
+++ V4/src/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         //int[] arr= {1,2,3,4,5,6,7,8,9};
         //System.out.println(Arrays.toString(Main.filter(arr, 3)));
-        System.out.println(Arrays.deepToString(hui(5)));
+        int[][] result1 = hui(5);
+        printHui(result1);
+        System.out.println("-------------------我是分割线-------------------");
+        int[][] result2 = hui(6);
+        printHui(result2);
     }
 
     public static int[] filter(int[] input, int n){  //以数组的形式输出input中为能整除n的数
@@ -35,52 +39,62 @@ public class Main {
         int j = 0;  //横轴
         // int[i][j]
         while (number <= bound){
-            switch (direction){
-                case "right":
-                    if(j < length && result[i][j] == 0){  //判断方向，边界，填没填
-                        result[i][j] = number;
-                        number++;
-                        j++;
-                        if(j >= length){
-                            direction = "down";
-                            j--;  // 防止越界
-                        }
-                    }
-                    break;
-                case "down":
-                    if(i < length && result[i][j] == 0){
-                        result[i][j] = number;
-                        number++;
-                        i++;
-                        if(i >= length){
-                            direction = "left";
-                            i--;
-                        }
-                    }
-                    break;
-                case "left":
-                    if(j >= 0 && result[i][j] == 0){
-                        result[i][j] = number;
-                        number++;
-                        j--;
-                        if(j < 0){
-                            direction = "up";
-                            j++;
-                        }
-                    }
-                    break;
-                case "up":
-                    if(i >= 0 && result[i][j] == 0){
-                        result[i][j] = number;
-                        number++;
-                        i--;
-                        if(i < 0){
-                            direction = "right";
-                            i++;
-                        }
-                    }
+
+            if(direction.equals("right") && j < length && result[i][j] == 0){  // 填充数字的条件
+            //判断方向，边界，填没填
+                result[i][j] = number;  //填充数字
+                number++;  //数字递增
+                j++;  //指针移动
+                if(j >= length || result[i][j] != 0){  // 切换到下一个方向的条件
+                // j>= length是为了防止index out of bound导致外层if语句中result[][]报错
+                    direction = "down";
+                    j--;  // 防止越界
+                    i++;  // 换到下一行
+                }
+            }
+
+            if(direction.equals("down") && i < length && result[i][j] == 0){
+                result[i][j] = number;
+                number++;
+                i++;
+                if(i >= length || result[i][j] != 0){
+                    direction = "left";
+                    i--;
+                    j--;
+                }
+            }
+
+            if(direction.equals("left") && j >= 0 && result[i][j] == 0){
+                result[i][j] = number;
+                number++;
+                j--;
+                if(j < 0 || result[i][j] != 0){
+                    direction = "up";
+                    j++;
+                    i--;
+                }
+            }
+
+            if(direction.equals("up") && i >= 0 && result[i][j] == 0){
+                result[i][j] = number;
+                number++;
+                i--;
+                if(i < 0 || result[i][j] != 0){
+                    direction = "right";
+                    i++;
+                    j++;
+                }
             }
         }
         return result;
+    }
+
+    public static void printHui(int[][] input){
+        for(int[] ints : input){
+            for(int i : ints){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
